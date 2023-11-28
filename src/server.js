2e00c4b1871d169b748f4daa4acf30345a5de2b6
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-11-28 14:26:29 +0900

    여러 브라우저 탭에 메시지 보여주기

--- src/server.js
+++ src/server.js
@@ -14,10 +14,15 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on('connection', (socket) => {
+  sockets.push(socket);
   console.log('Connected to Browser');
   socket.on('close', () => console.log('Disconnected from Browser'));
-  socket.on('message', (message) => console.log(`${message}`));
+  socket.on('message', (message) => {
+    sockets.forEach(aSocket => aSocket.send(`${message}`));
+  });
   socket.send('Hello!');
 });
 
